23d9c39d2c9c8606e0a4cd376c55a6f3a52dfb85
Starla Oden <starlaoh@gmail.com>
2018-02-02 11:53:08 -0800

    Created Taco Bell class

---
+++ LoggingKata/TacoBell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingKata
+{
+    class TacoBell
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+    }
+}
