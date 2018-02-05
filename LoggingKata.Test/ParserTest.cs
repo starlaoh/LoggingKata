a95fc0baf7f8b8af290d39196d4ddd1d82b59aa2
Starla Oden <starlaoh@gmail.com>
2018-02-05 12:09:26 -0800

    Updated tests

--- LoggingKata.Test/ParserTest.cs
+++ LoggingKata.Test/ParserTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace LoggingKata.Test
 {
@@ -13,16 +8,13 @@ namespace LoggingKata.Test
         [Test]
         public void ShouldReturnNullForNullOrEmptyString()
         {
-            //Arrange
             var empty = "";
             string nul = null;
             var parser = new TacoParser();
 
-            //Act
             var emptyResult = parser.Parse(empty);
             var nullResult = parser.Parse(nul);
 
-            //Assert
             Assert.IsNull(emptyResult, "Should return null for empty strings");
             Assert.IsNull(nullResult, "Should return null for null strings");
         }
@@ -30,47 +22,24 @@ namespace LoggingKata.Test
         [Test]
         public void ShouldReturnNullForNoLatAndLon()
         {
-            //Arrange
             var line = "-83.2342, Testing";
             var parser = new TacoParser();
 
-            //Act
             var result = parser.Parse(line);
 
-            //Assert
             Assert.IsNull(result, "Should return null unless we have both the lat and lon");
         }
 
         [Test]
         public void ShouldParseString()
         {
-            //Arrange
-            const string line = "-84.677017, 34.073638, \"Taco Bell Acwort... (Free trial * Add to Cart for a full POI info)";
+            const string line =
+                "-84.677017, 34.073638, \"Taco Bell Acwort... (Free trial * Add to Cart for a full POI info)";
             var parser = new TacoParser();
 
-            //Act
             var result = parser.Parse(line);
 
-            //Assert
             Assert.IsNotNull(result);
-        }
-
-        /*[Test]
-        public void ShouldNotParse()
-        {
-            //Arrange
-            var parser = new TacoParser();
-            var nonParseValues = new[] {null, "", "-84.67701, Testing", "Testing, -84.67701"};
-
-            //Act
-            foreach (var val in nonParseValues)
-            {
-                var result = parser.Parse(val);
-            }
-
-            //Assert
-            Assert.IsNull(result, $"{result} should be null");*/
         }
     }
 }
-
--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -8,52 +8,37 @@ namespace LoggingKata
 {
     class Program
     {
-        //Why do you think we use ILog?
-        private static readonly ILog Logger =
-            LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         static void Main(string[] args)
         {
-            Logger.Info("Log initialized");
-
             var file = Path.Combine(Environment.CurrentDirectory, "Taco_Bell-US-AL-Alabama.csv");
-
             var lines = File.ReadAllLines(file);
 
-            if (lines.Length == 0)
+            switch (lines.Length)
             {
-                //Log Error
-                return;
+                case 0:
+                    Logger.Error("Nothing to read from file.");
+                    return;
+                case 1:
+                    Logger.Warn("Only 1 line read from file.");
+                    return;
             }
 
-            if (lines.Length == 1)
-            {
-                //Log Warning
-                return;
-            }
-
-            Logger.Error("Nothing to read from file.");
-            Logger.Warn("Only 1 line read from file.");
-            Logger.Debug($"(lines.Length) lines read from file.");
-
-
-            // Create a new instance of your TacoParser class
             var parser = new TacoParser();
             var locations = lines.Select(parser.Parse).ToList();
 
-
-            // Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the furthest from each other.
-            ITrackable LocA = null;
-            ITrackable LocB = null;
+            ITrackable locA = null;
+            ITrackable locB = null;
             var distance = 0.0;
 
             foreach (var a in locations)
             {
-                var origin = new Coordinate {Latitude = a.Location.Latitude, Longitude = a.Location.Longitude};
+                var origin = new Coordinate { Latitude = a.Location.Latitude, Longitude = a.Location.Longitude };
 
                 foreach (var b in locations)
                 {
-                    var destination = new Coordinate {Latitude = b.Location.Latitude, Longitude = b.Location.Longitude};
+                    var destination = new Coordinate { Latitude = b.Location.Latitude, Longitude = b.Location.Longitude };
 
                     var dist = GeoCalculator.GetDistance(origin, destination);
 
@@ -63,10 +48,12 @@ namespace LoggingKata
                     }
 
                     distance = dist;
-                    LocA = a;
-                    LocB = b;
+                    locA = a;
+                    locB = b;
                 }
 
+                Console.WriteLine($"The two tacobells furthest from each other are {distance} miles apart.");
+                Console.WriteLine();
                 Console.ReadKey();
             }
         }
